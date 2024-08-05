9bfbe7a731a97dd1a1eccac9d572f88701799914
Retired64 <145419339+Retired64@users.noreply.github.com>
2024-08-05 00:48:53 -0700

    Add files via upload

--- rainbow/foto.js
+++ rainbow/foto.js
@@ -16,3 +16,22 @@ function nextImage() {
 function prevImage() {
   showImage(currentIndex - 1);
 }
+// JavaScript para el control del segundo carrusel
+let currentIndexTwo = 0;
+
+function showImageTwo(index) {
+  const images = document.querySelectorAll('.custom-carousel-image-two');
+  const totalImages = images.length;
+  currentIndexTwo = (index + totalImages) % totalImages;
+  const offset = -currentIndexTwo * 100;
+  document.querySelector('.custom-carousel-inner-two').style.transform = `translateX(${offset}%)`;
+}
+
+function nextImageTwo() {
+  showImageTwo(currentIndexTwo + 1);
+}
+
+function prevImageTwo() {
+  showImageTwo(currentIndexTwo - 1);
+}
+
